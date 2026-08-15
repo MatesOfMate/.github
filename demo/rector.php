02a5bc30b97527f50abe613459fcd00e9a6b53a4
Johannes Wachter <johannes@sulu.io>
2026-08-15 23:00:23 +0200

    Add Rector to the demo application
    - Install rector/rector so the rector-extension tools have a binary to drive
    - Add a rector.php targeting PHP 8.4 with code quality, dead code, early
      return, type declaration and PHPUnit sets
    - Apply the resulting refactorings to the test bootstrap and skipped test
    - Document the Rector commands, MCP tool mapping and configuration file

---
+++ demo/rector.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the MatesOfMate Organisation.
+ *
+ * (c) Johannes Wachter <johannes@sulu.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\SafeDeclareStrictTypesRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/mate/src',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withoutParallel()
+    ->withSkip([
+        __DIR__.'/vendor',
+        __DIR__.'/var',
+        SafeDeclareStrictTypesRector::class,
+    ])
+    ->withSets([
+        LevelSetList::UP_TO_PHP_84,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+        PHPUnitSetList::PHPUNIT_100,
+    ]);
